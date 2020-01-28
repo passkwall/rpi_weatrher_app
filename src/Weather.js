8bb25692aaf2175a8ccd91d16eb7c9541e636597
passkwall <apcipollone3@gmail.com>
2020-01-28 00:14:50 -0800

    date and time

--- src/Weather.js
+++ src/Weather.js
@@ -5,8 +5,6 @@ import './App.css';
 const WEATHER_API = "" // weatherapi key goes here
 const ZIP_CODE = "" // yup that too
 
-console.log(WEATHER_API); 
-
 async function getApiData(param) {
     var WEATHER_URL = ` http://api.weatherapi.com/v1/${param}.json?key=${WEATHER_API}`
     var response = await fetch(WEATHER_URL+`&q=${ZIP_CODE}`, {method: "GET"})
@@ -24,8 +22,8 @@ export class Weather extends Component {
     };
     
     resetPage() {
-        setTimeout("location.reload()", 1000000)
-        this.setState({date: new Date(Date.now()).toString()})
+        setTimeout("location.reload()", 100000000000000)
+        this.setState({date: new Date()})
     }
     
     componentWillMount() {        
@@ -34,7 +32,6 @@ export class Weather extends Component {
         
         Promise.all([currentWeatherData, forecastWeatherData]).then((results) => {
 
-            console.log(results);
             if (results[0].error) {
                 this.setState({
                     pageState: "err"
@@ -45,7 +42,7 @@ export class Weather extends Component {
                     pageState: "resolved",
                     location: results[0].location.name,
                     forecast: results[1],
-                    date: new Date(Date.now()).toString()
+                    date: new Date()
                 })
                 this.resetPage()
             }
@@ -67,10 +64,12 @@ export class Weather extends Component {
     }
     return ( 
         <div>
-            <img src={this.state.data.current.condition.icon} /> <br />
+            <img src={this.state.data.current.condition.icon} height="50%" width="50%"/> <br />
             Location: {this.state.location}, {this.state.data.location.region} <br />
-            Currently: {this.state.data.current.temp_f}*, {this.state.data.current.condition.text} <br />
-            Refreshed at: {this.state.date}
+            Currently: {this.state.data.current.temp_f}*, {this.state.data.current.condition.text} <br /> <br />
+            Refreshed at: <br />
+            Time:   {this.state.date.getHours()}:{this.state.date.getMinutes()} <br />
+            Date:   {this.state.date.getMonth() + 1}/{this.state.date.getDate()}/{this.state.date.getFullYear()}
         </div>
 
     )
